b6f0476736e1db1ee531a6ddb06d92eefefb4e37
Kal <info@kalacademy.org>
2019-05-19 10:15:53 -0700

    Service and transformation

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ICurrentWeather } from '../icurrent-weather';
+import { WeatherService } from '../weather/weather.service';
 
 @Component({
   selector: 'app-current-weather',
@@ -8,19 +9,12 @@ import { ICurrentWeather } from '../icurrent-weather';
 })
 export class CurrentWeatherComponent implements OnInit {
   current: ICurrentWeather
-  constructor() {
-    this.current = {
-      city: 'Bethesda',
-      country: 'US',
-      date: new Date(),
-      image: '',
-      temperature: 72,
-      description: 'Sunny'
-    }
-
-   }
+  constructor(private weatherService: WeatherService) {
+  }
 
   ngOnInit() {
+    this.weatherService.getCurrentWeather('Bethesda', 'US')
+    .subscribe(data => this.current = data);
   }
 
 }
